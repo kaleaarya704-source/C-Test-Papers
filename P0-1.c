e069a54216b23ad0f131eef8c8c13fc9ab84d321
kaleaarya704-source <kaleaarya704@gmail.com>
2025-12-09 20:19:53 +0530

    Add files via upload

---
+++ P0-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int r,c, i, j;
+    printf("Enter value of rows and columns : ");
+    scanf("%d %d", &r,&c);
+    int **a = (int **)malloc(r * sizeof(int *));
+    for ( i = 0; i < r; i++)
+    {
+        a[i] = (int *)malloc(c * sizeof(int));
+    }
+    printf("Enter elements :\n ");
+    for ( i = 0; i < r; i++)
+    {
+        for ( j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Matrix :\n");
+    for ( i = 0; i < r; i++)
+    {
+        for ( j = 0; j < c; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    for ( i = 0; i < r; i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+}
