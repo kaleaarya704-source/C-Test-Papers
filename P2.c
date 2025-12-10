3672238517bdbb3c2c6fe7049bd95a1eeabf630d
kaleaarya704-source <kaleaarya704@gmail.com>
2025-12-10 20:24:24 +0530

    Add files via upload

---
+++ P2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int r,c,i,j,sum=0;
+    int **a;
+    printf("Enter value of rows and columns : ");
+    scanf("%d %d",&r,&c);
+    a=(int**)malloc(r*sizeof(int));
+    for ( i = 0; i < r; i++)
+    {
+        a[i]=(int*)malloc(c*sizeof(int));
+    }
+    printf("Enter elements :\n");
+    for ( i = 0; i < r; i++)
+    {
+        for ( j = 0; j < c; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Matrix :\n");
+     for ( i = 0; i < r; i++)
+    {
+        for ( j = 0; j < c; j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for ( j = 0; j < c; j++)
+    {
+        sum+=a[0][j];
+    }
+    printf("Sum of 0th row : %d",sum);
+    for ( i = 0; i < r; i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+    return 0;
+}
