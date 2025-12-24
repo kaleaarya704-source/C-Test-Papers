49c428d22a93e0ce7264755a631927f1ca00861f
kaleaarya704-source <kaleaarya704@gmail.com>
2025-12-24 22:10:51 +0530

    Add files via upload

--- P1.c
+++ P1.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int r, c, num;
-    printf("Enter number of rows and columns: ");
-    scanf("%d %d", &r, &c);
+int main() {
+    int n;
+    printf("Enter size of nxn matrix: ");
+    scanf("%d", &n);
 
-    int **a = (int **)malloc(r * sizeof(int *));
-    for (int i = 0; i < r; i++)
-        a[i] = (int *)malloc(c * sizeof(int));
+    int **a = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
+        a[i] = (int *)malloc(n * sizeof(int));
+
+    int *arr = (int *)malloc(n * sizeof(int));
+    int *result = (int *)malloc(n * sizeof(int));
 
     printf("Enter elements of matrix:\n");
-    for (int i = 0; i < r; i++)
-        for (int j = 0; j < c; j++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             scanf("%d", &a[i][j]);
 
-    printf("Enter integer value: ");
-    scanf("%d", &num);
+    printf("Enter elements of array:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
 
-    printf("\nResultant matrix:\n");
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-            printf("%d\t", a[i][j] * num);
-        printf("\n");
+    // Multiply matrix and array
+    for (int i = 0; i < n; i++) {
+        result[i] = 0;
+        for (int j = 0; j < n; j++)
+            result[i] += a[i][j] * arr[j];
     }
 
-    for (int i = 0; i < r; i++)
+    printf("\nResultant array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d\t", result[i]);
+
+    // Free memory
+    for (int i = 0; i < n; i++)
         free(a[i]);
     free(a);
+    free(arr);
+    free(result);
 
     return 0;
 }
